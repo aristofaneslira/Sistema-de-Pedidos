a6d7f1d0f622b58f99cf7a577428300fac93d83f
Aristófanes Lira <31486514+aristofaneslira@users.noreply.github.com>
2023-05-31 10:12:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Rotas from 'pedidos/src/rotas.js';
-import "pedidos/src/style/global.css";
+import Rotas from 'Web/pedidos/src/rotas.js';
+import "Web/pedidos/src/style/global.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
