803fb4de31d4f5d0a57e181d7360376921fb8331
Aristófanes Lira <31486514+aristofaneslira@users.noreply.github.com>
2023-05-31 09:44:43 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Rotas from '.pedidos/src/rotas.js';
+import ".pedidos/src/style/global.css";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+   <Rotas />
+);
